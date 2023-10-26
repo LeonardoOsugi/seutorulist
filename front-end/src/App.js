764ad208192e6d495e92ef8c12cc8d9b490060f0
Leonardo Satoru Osugi <leoosugi96@gmail.com>
2023-10-25 22:21:44 -0300

    feat: add react-router-dom

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,12 @@
-
+import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 
 export default function App() {
   return (
-    <div className="App">
-      SR BUNDA CAGADA
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/sign-in" element={<p>Página inicial</p>}/>
+        <Route path="*" element={<Navigate to="/sign-in"/>}/>
+      </Routes>
+    </Router>
   );
 }
